88b62221f4c7286f242166e887505356a042fd05
Joao Pedro <joaoconta888@gmail.com>
2024-07-24 17:05:12 -0300

    Nova Aba atualizada

---
+++ js2.js
@@ -0,0 +1,216 @@
+function domestico() {
+    location.href = "index2.html"
+  }
+  
+  
+  function vestimenta() {
+    location.href = "index.html"
+  }
+  
+  
+  
+  
+  
+  
+  function findsking() {
+  
+  
+  
+      
+    location.href = 'https://www.instagram.com/findsking/?hl=pt'
+    
+  }
+
+
+
+  function produto1() {
+  
+  
+  
+      
+    location.href = 'https://amzn.to/4cRbaQY '
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function produto2() {
+  
+  
+  
+      
+    location.href = 'https://amzn.to/4bPVnk9 '
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function produto3() {
+  
+  
+  
+      
+    location.href = 'https://amzn.to/3zWNhZO'
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function produto4() {
+  
+  
+  
+      
+    location.href = 'https://amzn.to/3WzuPPA'
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+  
+
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function produto5() {
+  
+  
+  
+      
+    location.href = 'https://amzn.to/4cWy5KD '
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+  
